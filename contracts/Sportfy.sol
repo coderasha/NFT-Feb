64ff25c37da3d09de48fe2e12b93f2783da04417
coderasha <ashadipta@gmail.com>
2022-02-17 20:02:45 +0530

    import openzeppelin contracts

--- contracts/Sportfy.sol
+++ contracts/Sportfy.sol
@@ -1,2 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.5.0/utils/Counters.sol";
